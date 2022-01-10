af45f3c5de4c6e2714d41298cc28be2bfea1b430
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-01-10 09:29:53 -0400

    inicio del curso

---
+++ funciones.js
@@ -0,0 +1,12 @@
+// declarativas
+
+function miFuncion() {
+    return 3;
+}
+
+
+// Expresion
+
+var miFuncion = function(a,b){
+    return a + b;
+}
---
+++ variable.js
@@ -0,0 +1,18 @@
+var nombre = "ricardo";
+
+// declarar
+var edad;
+
+// inicializar
+edad = 30;
+
+edad;
+
+var elementos = ["computador", "Celular"];
+
+var persona = {
+    nombre: "Ricardo",
+    edad : 30,
+}
+
+persona
