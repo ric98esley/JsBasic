d9269f943344f9791fba8134ef7e5f2dc023d2a9
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-01-14 07:56:09 -0400

    recorrido de arrays

---
+++ recorrido_arrays.js
@@ -0,0 +1,40 @@
+var articulos = [
+	{ nombre: '📱', precio: 1000 },
+	{ nombre: '💻', precio: 1500 },
+	{ nombre: '🖥', precio: 2000 },
+	{ nombre: '⌨️', precio: 100 },
+	{ nombre: '🖱', precio: 70 },
+	{ nombre: '🚗', precio: 30000 },
+];
+
+// Método Filter
+var articulosFiltrados = articulos.filter(function(articulo) {
+	return articulo.precio <= 500;
+});
+
+// Método Map
+var nombreArticulos = articulos.map(function(articulo) {
+	return articulo.nombre;
+});
+
+articulosFiltrados;
+// (2) [{…}, {…}]
+//   0: {nombre: "⌨️", precio: 100}
+//   1: {nombre: "🖱", precio: 70}
+
+nombreArticulos; // (5) ["📱", "💻", "🖥", "⌨️", "🚗"]´
+
+//find Ayuda a encontrar algo dentro del array articulos
+var encuentraArticulo = articulos.find(function(articulo){
+    return articulo.nombre === "Laptop";
+});
+
+//forEach No es necesario generar nuevo array, se utiliza para realizar un recorrido de un array principal
+articulos.forEach(function(articulo){
+    console.log(articulo.nombre);
+});
+
+//some Se genera nuevo array, regresa un condición en Boolean
+var articulosBaratos = articulos.some(function(articulo){
+    return articulo.costo <= 700;
+});
